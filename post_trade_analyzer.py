adafaab0f08b8ffdec0b4eead9c3946e26bc0fe6
mohsen202566 <norozi100296269@gmail.com>
2026-06-29 11:05:06 +0330

    Delete post_trade_analyzer.py

--- post_trade_analyzer.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-class PostTradeAnalyzer:
-    def record_closed_signal(self, storage, signal_id: int) -> None:
-        storage.update_learning_from_signal(signal_id)
